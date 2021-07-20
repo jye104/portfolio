a8d6c1dd26762032bdd2aad005e7d9a3eb00dfd3
jye104 <jye104@nate.com>
2021-07-20 17:35:17 +0900

    20210720 17:35 수정

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,10 +1,18 @@
 $(document).ready(function() {
-  // 마우스 효과
+  // 마우스 커서 효과
   $('body').on('mousemove', function(e){
     // 스크롤바가 이동한 거리를 포함
     const mouseX = e.pageX;
     const mouseY = e.pageY;
-    console.log(mouseX, mouseY);
-    gsap.to('.cursor', {left: mouseX-150, top: mouseY-150, /* duration: 0.3 , delay: 0.1*/});
+    // console.log(mouseX, mouseY);
+    gsap.to('.cursor', {left: mouseX-100, top: mouseY-100});
+
+  // face - 마우스 효과
+    const eyeBall = $('.eyeball')
+    const eyeBallX = eyeBall.width()/2 + eyeBall.offset().left;
+    const eyeBallY = eyeBall.height()/2 + eyeBall.offset().top;
+
+    // gsap.to('.pupil', {left: (mouseX - eyeBallX) + 5 + 'px', top: (mouseY - eyeBallY) + 6 + 'px'});
   });
+  $('#intro').show(10)
 });
